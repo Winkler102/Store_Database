41ab20c209f0743d5045d9d10e5e57b6121b2934
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-06-10 12:01:00 -0500

    Filled in models and setup index

--- models/index.js
+++ models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'Product_Tags',
+  foreignKey: 'tag_id'
+})
+
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'Tags',
+  foreignKey: 'product_id'
+})
 
 module.exports = {
   Product,
